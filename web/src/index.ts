e740da4444103fc61938006932aefd1d41c8e7b4
geoffrey <geoffreylao@outlook.com>
2021-10-27 11:59:15 -0300

    drynamic array creation

---
+++ web/src/index.ts
@@ -0,0 +1,17 @@
+import { User } from "./models/User";
+
+const user = new User({ name: 'name', age: 2 });
+
+user.on('change', () => {
+
+})
+
+user.on('change', () => {
+
+})
+
+user.on('changer', () => {
+
+})
+
+console.log(user)
