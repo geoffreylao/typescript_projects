f7db637c7b3f831ba37c9bd0eaf4941774c49c38
geoffrey lao <geoffreylao@outlook.com>
2021-10-28 15:10:08 -0300

    options for adapting sync

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+
 import {Eventing} from './Eventing';
 
 interface UserProps {
@@ -23,23 +23,5 @@ export class User{
 
 
 
-  fetch(): void{
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse) =>{
-        this.set(response.data);
-      });
-  }
 
-  save(): void{
-
-    const id = this.get('id')
-
-    if(id){
-      //put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    }else{
-      //post
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
 }
