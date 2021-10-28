17615a218636c6dff2b07ea13947dfd3b2f04973
geoffrey <geoffreylao@outlook.com>
2021-10-28 17:59:49 -0300

    problems with get

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,25 +1,19 @@
-
+import { Sync } from './Sync';
 import {Eventing} from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000'
+
 export class User{
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) { }
-  
-  
-  get(propName: string) : number | string{
-    return this.data[propName];
-  }
 
-  set(update: UserProps): void{
-    Object.assign(this.data, update);
-  }
 
 
 
