326129b5e430ea805143fe9bcbf1c1709840f011
geoffrey <geoffreylao@outlook.com>
2021-11-03 20:01:48 -0300

    saving data from view

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,21 +1,29 @@
-import { User } from "../models/User";
+import { User, UserProps } from "../models/User";
+import { View } from "./Views";
 
-export class UserForm {
-  constructor(public parent: Element, public model: User){}
-
+export class UserForm extends View<User, UserProps>{
   eventsMap(): {[key: string]: () => void}{
     return {
-      'click:button' :  this.onButtonClick,
-      'mouseenter:h1' : this.onHeaderHover
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick
     };
   }
 
-  onHeaderHover(): void {
-    console.log('H1 was hovered over')
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
   }
 
-  onButtonClick(): void {
-    console.log('Hi there')
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input')
+
+    if (input) {
+      const name = input.value;
+
+      this.model.set({ name })
+    }
+ 
+
+
   }
 
   template(): string {
@@ -25,30 +33,10 @@ export class UserForm {
       <div>User name: ${this.model.get('name')}</div>
       <div>Age: ${this.model.get('age')}</div>
       <input />
-      <button>Click Me</button>
+      <button class="set-name">Change Name</button>
+      <button class="set-age">Set Random Age</button>
     </div>    
     `
   }
 
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap){
-      const [eventName, selector] = eventKey.split(':');
-
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  render(): void {
-    const templateElement = document.createElement('template');
-
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
-  }
 }
