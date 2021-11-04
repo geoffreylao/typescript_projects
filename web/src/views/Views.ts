dcae90541f32d385ae64dcd0be657b83b43324d9
geoffrey <geoffreylao@outlook.com>
2021-11-04 14:14:39 -0300

    app wrapup

--- web/src/views/Views.ts
+++ web/src/views/Views.ts
@@ -1,4 +1,3 @@
-import { User } from "../models/User";
 import {Model} from "../models/Model"
 
 interface ModelForView {
@@ -6,14 +5,24 @@ interface ModelForView {
 }
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.model.on('change', () => {
       this.bindModel();
     })
   }
 
-  abstract eventsMap(): { [key: string]: () => void }
+  
   abstract template(): string
+
+  regionsMap(): {[key: string]: string} {
+    return {}
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindModel(): void {
     this.model.on('change', () => {
@@ -33,6 +42,23 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector)
+      if (element) {
+        this.regions[key] = element
+      }
+      
+    }
+  }
+
+  onRender(): void {
+
+  }
+
   render(): void {
     this.parent.innerHTML = '';
 
@@ -41,6 +67,9 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
