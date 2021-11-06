fdd768f611bc4418e9176759ae3bb6983d2b9986
geoffrey <geoffreylao@outlook.com>
2021-11-06 15:13:23 -0300

    decorators

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,17 +1,47 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `this color ${this.color}`
   }
 
-  @testDecorator
-  pilot(): void {
-    console.log('swish');
+  @logError('boat sunk in ocean')
+  pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    if (speed === 'fast'){
+      console.log('swish')
+    } else {
+      console.log('nothing')
+    }
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key)
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor)
 }
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  //console.log(target);
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage)
+      }
+    }
+  }
+}
+
